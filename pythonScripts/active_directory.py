a8289bf6ff79264e46a0fff18ffa81041b98434b
Andrew Xie <ax6740@rit.edu>
2025-01-02 19:01:24 -0500

    basic domain controller promote function

--- pythonScripts/active_directory.py
+++ pythonScripts/active_directory.py
@@ -10,15 +10,42 @@ Purpose of Program: Set up Default Active Directory Configuration Settings based
 
 Some Additional Notes:
 - This program assumes the DNS Server is run on the the same machine as the Active Directory Server
+- Active Directory is run on Windows (duh)
+- Program HEAVILY relies on proper User usage and is only supposed to be used to make the setting up process faster
 """
 
 import subprocess
 import os
 import time
+import re
 
-# Install Active Directory ADUC, DHCP, DNS
-def install_tools():
-    pass
+# Install Active Directory ADUC, DHCP, DNS given a Domain Name to Promote Server to a Domain Controller
+def install_tools(dm):
+    try:
+        aduc = subprocess.run(["powershell", "-Command", "Install-WindowsFeature RSAT-ADDS"], capture_output=True, text=True, check=True)
+        dhcp = subprocess.run(["powershell", "-Command", "Install-WindowsFeature DHCP -IncludeManagementTools"], capture_output=True, text=True, check=True)
+        dns = subprocess.run(["powershell", "-Command", "Install-WindowsFeature DNS -IncludeManagementTools"], capture_output=True, text=True, check=True)
+        
+        print(aduc.stdout)
+        print(dhcp.stdout)
+        print(dns.stdout)
+        
+        # Assume Active Directory is the DNS Server for this program
+        ipconf = subprocess.check_output(["ipconfig", "/all"], text=True)
+        ip_addr = re.search(r"IPv4 Address\s*.*:\s*([\d\.]+)", ipconf)
+        
+        # Promote to Domain Controller
+        temp_command = f'Install-ADDSForest -DomainName "{dm}" -InstallDNS -Force'
+        promote = subprocess.run(["powershell", "-Command", temp_command], capture_output=True, text=True, check=True)
+        print(promote.stdout)
+        
+        # Authorize DHCP Server
+        temp_command = f'Add-DhcpServerInDC -DnsName "{dm}" -IpAddress {ip_addr}'
+        auth = subprocess.run(["powershell", "-Command", temp_command], capture_output=True, text=True, check=True)
+        print(auth.stdout)
+        
+    except subprocess.CalledProcessError as e:
+        print(f"Error in Installing ADUC, DHCP, and DNS: {e.stderr}")
 
 # Setup DHCP scope given parameters: Default Gateway IP, IP Scope, and Excluded IP scope
 def setup_dhcp(dg, ips, eips):
@@ -38,6 +65,10 @@ def create_GPO(ou):
 
 # Add specific functionality to a GPO
 def add_GPO_function(gpo):
+    pass
+
+# Simply like having a different function for the main menu
+def main_menu():
     pass
 
 def main():
